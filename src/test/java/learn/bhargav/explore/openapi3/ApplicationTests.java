8e51d4fee9518e39fea86a7c30d5fb74064b4d79
Bhargav Mummini <bhargav066@gmail.com>
2021-05-07 00:54:22 +0530

    first commit

---
+++ src/test/java/learn/bhargav/explore/openapi3/ApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.bhargav.explore.openapi3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
